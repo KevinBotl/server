343fc90d37b254ecd5958faedbe72843c20f45bc
Kevin <166707645+KevinBotl@users.noreply.github.com>
2024-04-21 18:24:50 +0800

    install.sh
    Minecraft server install

---
+++ install.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get upgrade
+apt-get install git -y
+apt-get install wget
+apt-get install unzip
+apt-cache search openjdk
+apt-get install openjdk-17-jre-headless
+wget https://drive.usercontent.google.com/download?id=1HclHU0QC0WuifYB41nU852J3tR5e-ysm&export=download&authuser=0&confirm=t&uuid=c7271c9c-6cfb-4e09-977b-8eef5d0a7e83&at=APZUnTXSLJkNhcP72Zh7uNGGrg_-:1713694604902
+unzip server.zip
+rm server.sh
